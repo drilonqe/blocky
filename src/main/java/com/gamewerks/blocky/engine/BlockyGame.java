d9819a8ce7297db6d43ab08757f55fa0ebd2374c
qerimidr <qerimidr@132.161.196.212>
2025-02-12 19:34:42 -0600

    Fixed missing break statement in 'processMovement'.

--- src/main/java/com/gamewerks/blocky/engine/BlockyGame.java
+++ src/main/java/com/gamewerks/blocky/engine/BlockyGame.java
@@ -1,10 +1,11 @@
 package com.gamewerks.blocky.engine;
 
+import static com.gamewerks.blocky.engine.Direction.LEFT;
+import static com.gamewerks.blocky.engine.Direction.NONE;
+import static com.gamewerks.blocky.engine.Direction.RIGHT;
 import com.gamewerks.blocky.util.Constants;
 import com.gamewerks.blocky.util.Position;
-
 import java.util.Random;
-
 
 public class BlockyGame {
     private static final int LOCK_DELAY_LIMIT = 30;
@@ -13,7 +14,6 @@ public class BlockyGame {
     private Piece activePiece;
     private Direction movement;
     
-
     private PieceKind[] pieceType; //added
     private int pieceIndex; // added
     
@@ -41,31 +41,22 @@ public class BlockyGame {
         
     }
     
-
-    private int lockCounter;
-    
     public BlockyGame() {
         board = new Board();
         movement = Direction.NONE;
-
+        lockCounter = 0;
         pieceInitialize();
-
-
         trySpawnBlock();
     }
     
     private void trySpawnBlock() {
         if (activePiece == null) {
-
             if(pieceIndex >= pieceType.length) {
                 shufflePieceTypes(); // we shuffle the piece types when all pieces used
                 pieceIndex = 0;
             }
          activePiece = new Piece(pieceType[pieceIndex], new Position(3, Constants.BOARD_WIDTH / 2 - 2));
          pieceIndex++;
-
-            activePiece = new Piece(PieceKind.I, new Position(Constants.BOARD_HEIGHT - 1, Constants.BOARD_WIDTH / 2 - 2));
-
             if (board.collides(activePiece)) {
                 System.exit(0);
             }
@@ -83,9 +74,7 @@ public class BlockyGame {
             break;
         case RIGHT:
             nextPos = activePiece.getPosition().add(0, 1);
-
             break; // added missing break
-
         default:
             throw new IllegalStateException("Unrecognized direction: " + movement.name());
         }
@@ -95,11 +84,7 @@ public class BlockyGame {
     }
     
     private void processGravity() {
-
         Position nextPos = activePiece.getPosition().add(1, 0); // gravity is down
-
-        Position nextPos = activePiece.getPosition().add(-1, 0);
-
         if (!board.collides(activePiece.getLayout(), nextPos)) {
             lockCounter = 0;
             activePiece.moveTo(nextPos);
@@ -120,9 +105,7 @@ public class BlockyGame {
     
     public void step() {
         trySpawnBlock();
-
         processMovement(); // added this line
-
         processGravity();
         processClearedLines();
     }
